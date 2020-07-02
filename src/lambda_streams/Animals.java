0e3ac4550c72f2be03aec59d36200c5e8919bbdf
George Torres <torresgeorge2014@gmail.com>
2020-07-02 12:52:41 -0500

    Completing Exercise 2 for Java Module 8.

--- src/lambda_streams/Animals.java
+++ src/lambda_streams/Animals.java
@@ -1,10 +1,16 @@
 package lambda_streams;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Animals {
     //This class will be using streams.
-    static List<String> animals = Arrays.asList("peacoCK","rabbit","chiwawa","OranguTAN","vipeR","cobra","paNDa","bUffalo","DeeR","maLLard");
+    static List<String> animals = new LinkedList<>(Arrays.asList("peacoCK","rabbit","chiwawa","OranguTAN","vipeR",
+        "cobra","paNDa","bUffalo","DeeR","maLLard"));
 
     public static void main(String[] args) {
         System.out.println("original animals : " + animals);
@@ -26,36 +32,78 @@ class Animals {
         List<String> lowered = lowerFirst(animals,false);
         System.out.println(lowered);
 
-        
+        System.out.println(flipAnimals(true));
+        System.out.println(sortAnimals(true));
 
     }
 
     static List<String> capsFirst(List<String> animaList, boolean mutate) {
-        //clean up the animals list so that the first letter is capitalized, and all the other letters are lowercased. Use a stream to accomplish this task.  Also, the 2nd parameter of this function is a boolean.  use this boolean 'flag' to determine whether or not to 'mutate' the original animals array stored as a static class field.  if the flag is set to 'true', mutate the animals and return the animals out of the function.  if it is false, create a copy of the animals, perform your stream operations on the copy, and return the copy of animals out of the function, WITHOUT modifying the original animals array.  
-        return animals;
+        //clean up the animals list so that the first letter is capitalized, and all the other letters are lowercased.
+        //Use a stream to accomplish this task.  Also, the 2nd parameter of this function is a boolean.  use this
+        //boolean 'flag' to determine whether or not to 'mutate' the original animals array stored as a static class
+        //field.  if the flag is set to 'true', mutate the animals and return the animals out of the function.  if it
+        //is false, create a copy of the animals, perform your stream operations on the copy, and return the copy of
+        //animals out of the function, WITHOUT modifying the original animals array.
+//        return animals;
+        if(mutate) {
+            return animals =
+                    animals.stream().map(name -> Character.toUpperCase(name.charAt(0)) + name.substring(1).toLowerCase()).collect(Collectors.toList());
+        }
+        List<String> cloned = new ArrayList(animals);
+        return cloned.stream().map(name -> Character.toUpperCase(name.charAt(0)) + name.substring(1).toLowerCase()).collect(Collectors.toList());
     }
 
     static String addAnimal(String animal) {
         //add an animal to the animal list.
+        animals.add(animal);
         return animal;
     };
 
 
     static List<String> lowerFirst(List<String> animaList, boolean mutate) {
-        //lowercase the first letter, and uppercase the rest of the letters, using streams.  Also, depending on the value of the boolean flag 'mutate', mutate the original animals list, or perform your stream operations on a 'copy' of the animals list.  return the list out of hte function in both cases.
-        return animaList;
+        //lowercase the first letter, and uppercase the rest of the letters, using streams.  Also, depending on the
+        //value of the boolean flag 'mutate', mutate the original animals list, or perform your stream operations on
+        // a 'copy' of the animals list.  return the list out of hte function in both cases.
+        if(mutate) {
+            return animals.stream().map(name -> Character.toLowerCase(name.charAt(0)) + name.substring(1).toUpperCase()).collect(Collectors.toList());
+        }
+        List<String> cloned = new ArrayList(animals);
+        return cloned.stream().map(name -> Character.toLowerCase(name.charAt(0)) + name.substring(1).toUpperCase()).collect(Collectors.toList());
     }
 
     static List<String> flipAnimals(boolean mutate) {
-        //reverse the order of the animals in the animal list.  If the booleaen parameter is true, reverse the static animals array list by mutating the array.  if the mutate boolean is false, flip a 'copy' of the animals list, then return that list of flipped animals, WITHOUT mutating the static animals array. return the flipped list in both cases.
+        //reverse the order of the animals in the animal list.  If the boolean parameter is true, reverse the static
+        // animals array list by mutating the array.  if the mutate boolean is false, flip a 'copy' of the animals
+        // list, then return that list of flipped animals, WITHOUT mutating the static animals array. return the
+        // flipped list in both cases.
 
         List<String> instructions = Arrays.asList("Flip","the","animals","list","."," ","Mutate","the","animals","only","if","flag","true");
+
+        IntStream.range(0, instructions.size()).forEach((i) -> {
+            int j = instructions.size() - 1;
+            while(true) {
+                if (i < j) {
+                    String temp = instructions.get(i);
+                    instructions.set(i, instructions.get(j));
+                    instructions.set(j, temp);
+                    j--;
+                } else {
+                    break;
+                }
+            }
+        });
         return instructions;
     }
 
     static List<String> sortAnimals(boolean mutate) {
-    //sort the animals in alphabetical order.  If the booleaen parameter is true, mutating the animals list.  if the mutate boolean is false, sort a 'copy' of the animals list, then return that list of sorted animals, WITHOUT mutating the static animals array. return the sorted list in both cases.
-    return animals;
+        //sort the animals in alphabetical order.  If the boolean parameter is true, mutating the animals list.  if the
+        // mutate boolean is false, sort a 'copy' of the animals list, then return that list of sorted animals,
+        // WITHOUT mutating the static animals array. return the sorted list in both cases.
+        if(mutate) {
+            return animals = animals.stream().sorted().collect(Collectors.toList());
+        }
+        List<String> cloned = new ArrayList(animals);
+        return cloned.stream().sorted().collect(Collectors.toList());
     }
 
 
--- src/lambda_streams/Numbers.java
+++ src/lambda_streams/Numbers.java
@@ -2,15 +2,42 @@ package lambda_streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 class Numbers {
     static List<Integer> nums = Arrays.asList(10,100,1000,5,50,500,3,30,300,7,70,700,1,10,100,25,250,2500);
 
     public static void main(String[] args) {
-        //Part I :complete the static class methods that have been set up in this Numbers class java file.  Use streams to compute the results wheever possible.
+        //Part I :complete the static class methods that have been set up in this Numbers class java file.  Use
+        // streams to compute the results wherever possible.
         System.out.println(nums);
+        System.out.println(added());
+        System.out.println(subtracted());
+        System.out.println(multiplied());
+        System.out.println(divided());
+        System.out.println(findMax());
+        System.out.println(findMin());
+        System.out.println(isOdd(1));
+        System.out.println(isEven(1));
+        System.out.println(isPrime(6));
 
-        //Part II - refactor all of the class methods to accept lambda expressions. You can put the lambda functions directly inside the method calls, or defined them first, then pass them into the methods. give them the same names as the static methods, but add the word 'lambda' in front of every lambda function:
+        //Part II - refactor all of the class methods to accept lambda expressions. You can put the lambda functions
+        // directly inside the method calls, or defined them first, then pass them into the methods. give them the
+        // same names as the static methods, but add the word 'lambda' in front of every lambda function:
+
+//        System.out.println(lambdaIs(1, new IntPredicate() {
+//
+//            @Override
+//            public boolean test(int value) {
+//                if (value%2 != 0) {
+//                    return true;
+//                }
+//                return false;
+//            }
+//        }));
+
+//        System.out.println(lambdaIs(() -> nums.get(3)%2 != 0 ? true : false));
+
         /* e.g.
 
         added(() -> {});
@@ -34,52 +61,82 @@ class Numbers {
 
     static boolean isOdd(int i) {
         //determine if the value at the index i is odd.  return true if yes, return false if  no.
-        return false;
+        Stream<Integer> stream = Arrays.asList(nums.get(i)).stream();
+
+        return stream.allMatch((Integer element) -> element % 2 != 0);
     }
 
     static boolean isEven(int i) {
         //determine if the value at the index i is even.  return true if yes, return false if  no.
-        return false;
+        Stream<Integer> stream = Arrays.asList(nums.get(i)).stream();
+
+        return stream.allMatch((Integer element) -> element % 2 == 0);
     }
 
     static boolean isPrime(int i) {
-         //determine if the value at the index i is a prime number.  return true if yes, return false if no.
-         return false;
+        //determine if the value at the index i is a prime number.  return true if yes, return false if no.
+        Stream<Integer> stream = Arrays.asList(nums.get(i)).stream();
+
+        return stream.allMatch((Integer element) -> {
+            if (nums.get(element) <= 1) {
+                return false;
+            }
+
+            for (int x = 2; x < nums.get(element); x++)
+                if (nums.get(element) % x == 0) {
+                    return false;
+                }
+            return true;});
+
+
     }
 
     static int added() {
-        //add all the elements in the list.  return the sum.  
-        return 0;
+        //add all the elements in the list.  return the sum.
+        int total = nums.stream().mapToInt(num -> num).sum();
+        return total;
     }
 
     static int subtracted() {
         //subtract all the elements in the list. return the remainder.
-        return 0;
+        int subtraction = nums.stream().reduce(nums.get(0), (a, b) -> a - b);
+        return subtraction;
     }
 
-    static int multipled() {
+    static int multiplied() {
         //multiply all the elements in the list. and return the product.
-        return 0;
+        //Value comes out to be a negative because the Integer List type does not support the size of the number.
+        int multiplication = nums.stream().reduce(nums.get(0), (a, b) -> a*b);
+        return multiplication;
     }
 
     static int divided() {
         //multiply all the elements in the list. and return the product.
-        return 0;
+        //^^Guessing those directions meant to say divide. Returns 0 since number is too small for int.
+        int division = nums.stream().reduce(nums.get(0), (a, b) -> a/b);
+        return division;
     }
 
     static int findMax() {
          //return the maximum value in the list.
-        return 0;
+        Integer max = nums.stream().max(Integer::compare).get();
+        return max;
     }
 
     static int findMin() {
         //return the minimum value in the list.
-        return 0;
+        Integer min = nums.stream().min(Integer::compare).get();
+        return min;
     }
 
     static int compare(int i, int j) {
         //compare the values stored in the array at index position i and j.  
         //if the value at i is greater, return 1.  if the value at j is greater, return -1.  if the two values are equal, return 0.
+        Stream<Integer> stream = Arrays.asList(nums.get(i), nums.get(j)).stream();
+
+        stream.forEach((Integer element) -> {
+
+        });
         return 0;
     }
 
@@ -87,5 +144,15 @@ class Numbers {
         //add a new value to the values list. return that value after adding it to the list.
         return 0;
     }
+
+
+    static boolean lambdaIs(int i) {
+        return false;
+    }
+
+    interface lambdaIs {
+
+    }
+
 
 }
